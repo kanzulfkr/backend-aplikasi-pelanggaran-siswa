73f664e43ec5fba6fc3d8062f7a710e870b4a8fa
kanzul <muhammad.mf585@gmail.com>
2024-01-12 14:22:41 +0700

    fix api violation-types

--- routes/api.php
+++ routes/api.php
@@ -23,5 +23,5 @@ Route::delete('/violations/delete/{violation}', [ViolationController::class, 'de
 
 Route::get('/violations-types', [ViolationTypeController::class, 'index'])->middleware('auth:sanctum');
 Route::post('/violations-types/store', [ViolationTypeController::class, 'store'])->middleware('auth:sanctum');
-Route::put('/violations-types/update/{violation}', [ViolationTypeController::class, 'update'])->middleware('auth:sanctum');
-Route::delete('/violations-types/delete/{violation}', [ViolationTypeController::class, 'destroy'])->middleware('auth:sanctum');
+Route::put('/violations-types/update/{violations_types}', [ViolationTypeController::class, 'update'])->middleware('auth:sanctum');
+Route::delete('/violations-types/delete/{violations_types}', [ViolationTypeController::class, 'destroy'])->middleware('auth:sanctum');
